ba40509d878a9a4c710fcdf47e3a417ea46261a3
Isha Narola <ishanarola123@gmail.com>
2022-12-03 22:30:29 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,374 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous" />
+    <link rel="stylesheet" href="styles/style.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm" crossorigin="anonymous">
+    <!-- <link rel="stylesheet" href="styles/s.css"> -->
+    <link rel="stylesheet" href="styles/4.css">
+
+
+
+    <script src="https://cdn.tailwindcss.com?plugins=forms,typography,aspect-ratio,line-clamp"></script>
+    <title>Gift Recommendation</title>
+
+
+</head>
+
+<body>
+
+
+
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+
+        <a class="navbar-brand" href="#">
+            <img src="./logo (3).png" width="30" height="30" alt=""></a>
+
+        <a class="navbar-brand" href="#">Giftrec.com</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">Home<span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Day special</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">History</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">About</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Feedback</a>
+                </li>
+            </ul>
+            <form class="form-inline">
+                <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+            </form>
+
+            <button type="button" class="btn btn-light mx-2 py-2">Login</button>
+
+        </div>
+    </nav>
+
+
+
+    <!-- slider -->
+    <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+        <ol class="carousel-indicators">
+            <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+        </ol>
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <img class="d-block w-100" src="./images/gift.png" alt="First slide">
+            </div>
+            <div class="carousel-item">
+                <img class="d-block w-100" src="./images/gift (2).png" alt="Second slide">
+            </div>
+            <div class="carousel-item">
+                <img class="d-block w-100" src="./images/gift (3).png" alt="Third slide">
+            </div>
+        </div>
+        <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+        </a>
+    </div>
+
+    <p class="h1">Find gift ideas from here!</p>
+
+    <!-- grid -->
+
+    <!-- <div class="container">
+        <div class="row">
+            <div class="col-sm">
+                <div class="cards">
+                    <img class="op" src="https://cdn.pixabay.com/photo/2015/06/26/13/49/father-822550__340.jpg">
+                    <div class="img">
+                        <img class="im" src="https://cdn.pixabay.com/photo/2015/06/26/13/49/father-822550__340.jpg">
+                    </div>
+                    <div class="content">
+                        <div class="details">
+                            <h2>Father</h2>
+                            <ul class="icons">
+                                <li><a href="#"><i class="fas fa-house-user"></i></a></li>
+                                <li><a href="#"><i class="fas fa-house-user"></i></a></li>
+                                <li><a href="#"><i class="fas fa-house-user"></i></a></li>
+                                <li><a href="#"><i class="fas fa-house-user"></i></a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm">
+                <div class="cards">
+                    <img class="op" src="https://cdn.pixabay.com/photo/2015/06/26/13/49/father-822550__340.jpg">
+                    <div class="img">
+                        <img class="im" src="https://cdn.pixabay.com/photo/2015/06/26/13/49/father-822550__340.jpg">
+                    </div>
+                    <div class="content">
+                        <div class="details">
+                            <h2>Father</h2>
+                            <ul class="icons">
+                                <li><a href="#"><i class="fas fa-house-user"></i></a></li>
+                                <li><a href="#"><i class="fas fa-house-user"></i></a></li>
+                                <li><a href="#"><i class="fas fa-house-user"></i></a></li>
+                                <li><a href="#"><i class="fas fa-house-user"></i></a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm">
+                <div class="cards">
+                    <img class="op" src="https://cdn.pixabay.com/photo/2015/06/26/13/49/father-822550__340.jpg">
+                    <div class="img">
+                        <img class="im" src="https://cdn.pixabay.com/photo/2015/06/26/13/49/father-822550__340.jpg">
+                    </div>
+                    <div class="content">
+                        <div class="details">
+                            <h2>DEMO</h2>
+                            <ul class="icons">
+                                <li><a href="#"><i class="fas fa-house-user"></i></a></li>
+                                <li><a href="#"><i class="fas fa-house-user"></i></a></li>
+                                <li><a href="#"><i class="fas fa-house-user"></i></a></li>
+                                <li><a href="#"><i class="fas fa-house-user"></i></a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm">
+                <div class="cards">
+                    <img class="op" src="https://cdn.pixabay.com/photo/2015/06/26/13/49/father-822550__340.jpg">
+                    <div class="img">
+                        <img class="im" src="https://cdn.pixabay.com/photo/2015/06/26/13/49/father-822550__340.jpg">
+                    </div>
+                    <div class="content">
+                        <div class="details">
+                            <h2>DEMO</h2>
+                            <ul class="icons">
+                                <li><a href="#"><i class="fas fa-house-user"></i></a></li>
+                                <li><a href="#"><i class="fas fa-house-user"></i></a></li>
+                                <li><a href="#"><i class="fas fa-house-user"></i></a></li>
+                                <li><a href="#"><i class="fas fa-house-user"></i></a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm">
+                <div class="cards">
+                    <img class="op" src="https://cdn.pixabay.com/photo/2015/06/26/13/49/father-822550__340.jpg">
+                    <div class="img">
+                        <img class="im" src="https://cdn.pixabay.com/photo/2015/06/26/13/49/father-822550__340.jpg">
+                    </div>
+                    <div class="content">
+                        <div class="details">
+                            <h2>DEMO</h2>
+                            <ul class="icons">
+                                <li><a href="#"><i class="fas fa-house-user"></i></a></li>
+                                <li><a href="#"><i class="fas fa-house-user"></i></a></li>
+                                <li><a href="#"><i class="fas fa-house-user"></i></a></li>
+                                <li><a href="#"><i class="fas fa-house-user"></i></a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm">
+                <div class="cards">
+                    <img class="op" src="https://cdn.pixabay.com/photo/2015/06/26/13/49/father-822550__340.jpg">
+                    <div class="img">
+                        <img class="im" src="https://cdn.pixabay.com/photo/2015/06/26/13/49/father-822550__340.jpg">
+                    </div>
+                    <div class="content">
+                        <div class="details">
+                            <h2>DEMO</h2>
+                            <ul class="icons">
+                                <li><a href="#"><i class="fas fa-house-user"></i></a></li>
+                                <li><a href="#"><i class="fas fa-house-user"></i></a></li>
+                                <li><a href="#"><i class="fas fa-house-user"></i></a></li>
+                                <li><a href="#"><i class="fas fa-house-user"></i></a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm">
+                <div class="cards">
+                    <img class="op" src="https://cdn.pixabay.com/photo/2015/06/26/13/49/father-822550__340.jpg">
+                    <div class="img">
+                        <img class="im" src="https://cdn.pixabay.com/photo/2015/06/26/13/49/father-822550__340.jpg">
+                    </div>
+                    <div class="content">
+                        <div class="details">
+                            <h2>DEMO</h2>
+                            <ul class="icons">
+                                <li><a href="#"><i class="fas fa-house-user"></i></a></li>
+                                <li><a href="#"><i class="fas fa-house-user"></i></a></li>
+                                <li><a href="#"><i class="fas fa-house-user"></i></a></li>
+                                <li><a href="#"><i class="fas fa-house-user"></i></a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm">
+                <div class="cards">
+                    <img class="op" src="https://cdn.pixabay.com/photo/2015/06/26/13/49/father-822550__340.jpg">
+                    <div class="img">
+                        <img class="im" src="https://cdn.pixabay.com/photo/2015/06/26/13/49/father-822550__340.jpg">
+                    </div>
+                    <div class="content">
+                        <div class="details">
+                            <h2>DEMO</h2>
+                            <ul class="icons">
+                                <li><a href="#"><i class="fas fa-house-user"></i></a></li>
+                                <li><a href="#"><i class="fas fa-house-user"></i></a></li>
+                                <li><a href="#"><i class="fas fa-house-user"></i></a></li>
+                                <li><a href="#"><i class="fas fa-house-user"></i></a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div> -->
+    <div class="suggestion">
+        <div class="raw">
+            <img src="images/2.jpg" alt="" style="width: 20rem;" class="child">
+            <div class="text">
+                <h3> Father </h3>
+                <p>
+                    <button type="button" class="btn btn-outline-danger">Birthday</button>&nbsp;
+                    <button type="button" class="btn btn-outline-warning">Aniversary</button><br>&nbsp;<br>
+                    <button type="button" class="btn btn-outline-success">Good Luck</button>&nbsp;
+                    <button type="button" class="btn btn-outline-primary">Congratulations</button>
+                </p>
+            </div>
+        </div>
+        <div class="raw">
+            <img src="images/2.jpg" alt="" style="width: 20rem;" class="child">
+            <div class="text">
+                <h3> Mother </h3>
+                <p>
+                    <button type="button" class="btn btn-outline-danger">Birthday</button>&nbsp;
+                    <button type="button" class="btn btn-outline-warning">Aniversary</button><br>&nbsp;<br>
+                    <button type="button" class="btn btn-outline-success">Good Luck</button>&nbsp;
+                    <button type="button" class="btn btn-outline-primary">Congratulations</button>
+            </div>
+        </div>
+        <div class="raw">
+            <img src="images/2.jpg" alt="" style="width: 20rem;" class="child">
+            <div class="text">
+                <h3> Husband </h3>
+                <p>
+                    <button type="button" class="btn btn-outline-danger">Birthday</button>&nbsp;
+                    <button type="button" class="btn btn-outline-warning">Engaugement</button>
+                    <button type="button" class="btn btn-outline-success">Aniversary</button><br><br>
+                    <button type="button" class="btn btn-outline-primary">Sorrow</button>
+                    <button type="button" class="btn btn-outline-light">Wedding</button>
+                    <button type="button" class="btn btn-outline-secondary">Valentine</button>
+                </p>
+            </div>
+        </div>
+        <div class="raw">
+            <img src="images/2.jpg" alt="" style="width:20rem;" class="child">
+            <div class="text">
+                <h3> Wife </h3>
+                <p>
+                    <button type="button" class="btn btn-outline-danger">Birthday</button>&nbsp;
+                    <button type="button" class="btn btn-outline-warning">Engaugement</button>
+                    <button type="button" class="btn btn-outline-success">Aniversary</button><br><br>
+                    <button type="button" class="btn btn-outline-primary">Sorrow</button>
+                    <button type="button" class="btn btn-outline-light">Wedding</button>
+                    <button type="button" class="btn btn-outline-secondary">Valentine</button>
+                </p>
+            </div>
+        </div>
+
+        <div class="raw">
+            <img src="images/2.jpg" alt="">
+            <div class="text">
+                <h3> Brother </h3>
+                <p>
+                    <button type="button" class="btn btn-outline-danger">Birthday</button>&nbsp;
+                    <button type="button" class="btn btn-outline-warning">Wedding</button><br>&nbsp;<br>
+                    <button type="button" class="btn btn-outline-success">Happy Journey</button>&nbsp;
+                    <button type="button" class="btn btn-outline-primary">Achivement</button>
+                    <button type="button" class="btn btn-outline-light">Rakhi</button>
+                </p>
+            </div>
+        </div>
+        <div class="raw">
+            <img src="images/2.jpg" alt="">
+            <div class="text">
+                <h3> Sister </h3>
+                <p>
+                    <button type="button" class="btn btn-outline-danger">Birthday</button>&nbsp;
+                    <button type="button" class="btn btn-outline-warning">Wedding</button><br>&nbsp;<br>
+                    <button type="button" class="btn btn-outline-success">Happy Journey</button>
+                    <button type="button" class="btn btn-outline-primary">Achivement</button>
+                </p>
+            </div>
+        </div>
+        <div class="raw">
+            <img src="images/2.jpg" alt="">
+            <div class="text">
+                <h3> Friends </h3>
+                <p>
+                    <button type="button" class="btn btn-outline-danger">Birthday</button>&nbsp;
+                    <button type="button" class="btn btn-outline-warning">Wedding</button><br>&nbsp;<br>
+                    <button type="button" class="btn btn-outline-success">Achivement</button>&nbsp;
+                    <button type="button" class="btn btn-outline-primary">Bachloar's Party</button>
+                </p>
+            </div>
+        </div>
+        <div class="raw">
+            <img src="images/2.jpg" alt="">
+            <div class="text">
+                <h3> Kids </h3>
+                <p>
+                    <button type="button" class="btn btn-outline-warning">Birthday</button><br>&nbsp;<br>
+                    <button type="button" class="btn btn-outline-success">First Day of School</button>
+                </p>
+            </div>
+        </div>
+    </div>
+    <br><br><br>
+
+   
+
+  
+
+
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+</body>
+
+</html>
